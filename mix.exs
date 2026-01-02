fdd5381c445800862fb9466d7e60ca3924ba6a00
gsarwate <4589380+gsarwate@users.noreply.github.com>
2026-01-01 23:39:15 -0800

    configure NimblePublisher

--- mix.exs
+++ mix.exs
@@ -64,7 +64,10 @@ defmodule Postbits.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:nimble_publisher, "~> 1.0"},
+      {:makeup_elixir, ">= 0.0.0"},
+      {:makeup_erlang, ">= 0.0.0"}
     ]
   end
 
